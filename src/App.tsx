195a35c93da569fcc2de94a0baa423287f8500e4
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-07-17 23:52:47 +0530

    demonstrate accessing state hooks by zustand outside a react component
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
 import { useCounterStore } from "./store";
 
+// Accessing outside a react component
+const logCount = () => {
+  const count = useCounterStore.getState().count;
+  console.log("Count: ", count);
+};
+
+const setCount = (value: number) => {
+  useCounterStore.setState({ count: value });
+};
+
 function App() {
+  // Accessing inside a React component
   const count = useCounterStore((state) => state.count);
   const incrementAsync = useCounterStore((state) => state.incrementAsync);
   const decrement = useCounterStore((state) => state.decrement);
+
+  useEffect(() => {
+    logCount();
+    setCount(4);
+    logCount();
+  }, []);
 
   return (
     <>
